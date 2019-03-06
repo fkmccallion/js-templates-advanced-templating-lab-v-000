78f75875eab6446ed58acab03a6368462ccc9ced
Keith <fkmccallion@gmail.com>
2019-03-06 06:46:08 -0800

    begin writing templates

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  formTemplate = document.getElementById('recipe-form-template').innerHTML;
+  document.getElementsByTagName('main')[0].innerHTML = formTemplate;
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function handleSubmit() {
+  let recipeName = document.getElementById('recipeName').value;
+  let recipeDescription = document.getElementById('recipeDescription').value;
+
+  var recipe = {
+    name: recipeName,
+    description: recipeDescription
+  }
+
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var templateResult = template(recipe);
+  document.getElementsByTagName('main')[0].innerHTML = templateResult;
+}
